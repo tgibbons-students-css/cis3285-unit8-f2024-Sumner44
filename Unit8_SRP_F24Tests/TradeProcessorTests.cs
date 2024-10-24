7774883e54c084b6e8f26851f88a127e57663358
Sumner Cotton <scotton@css.edu>
2024-10-24 11:43:20 -0500

    Added data base, learning exercise code

---
+++ Unit8_SRP_F24Tests/TradeProcessorTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SingleResponsibilityPrinciple;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SingleResponsibilityPrinciple.Tests
+{
+    [TestClass()]
+    public class TradeProcessorTests
+    {
+        private int CountDbRecords()
+        {
+            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\scotton\Documents\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30"))
+            //using (var connection = new System.Data.SqlClient.SqlConnection("Data Source=(local);Initial Catalog=TradeDatabase;Integrated Security=True;"))
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                string myScalarQuery = "SELECT COUNT(*) FROM trade";
+                SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
+                myCommand.Connection.Open();
+                int count = (int)myCommand.ExecuteScalar();
+                connection.Close();
+                return count;
+            }
+        }
+        [TestMethod()]
+        public void ProcessTradesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
